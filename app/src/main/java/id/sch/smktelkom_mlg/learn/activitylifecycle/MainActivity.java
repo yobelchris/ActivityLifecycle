9084b9e87c40221dd019b6530b1ce9a901ba97f9
yobelchris <yobelchris@gmail.com>
2016-10-13 21:48:32 +0700

    Lifecycle Music

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylifecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylifecycle/MainActivity.java
@@ -1,16 +1,20 @@
 package id.sch.smktelkom_mlg.learn.activitylifecycle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LifecycleTag";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
 
         Log.d(TAG, "onCreate: ");
     }
@@ -25,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: ");
+        mediaPlayer.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
+        mediaPlayer.pause();
     }
 
     @Override
@@ -49,5 +55,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
     }
 }
