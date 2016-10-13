c2e25b03caaa07e63792a63b01b4e07ab89de393
yobelchris <yobelchris@gmail.com>
2016-10-13 21:36:37 +0700

    Lifecycle Basic

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylifecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylifecycle/MainActivity.java
@@ -2,12 +2,52 @@ package id.sch.smktelkom_mlg.learn.activitylifecycle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "LifecycleTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
